1b53a69339cb1250b706c1dd392e5ec9c89ac819
joh-sch <johannes@schmoll.studio>
2023-03-28 10:25:28 +0200

    impl. removing of specific comp. instances by passing ID of comp. DOM element to removeComponents()

--- src/removeComponents.js
+++ src/removeComponents.js
@@ -6,8 +6,23 @@ import destroyInstance from "./destroyInstance";
  * @param context: DOM element
  */
 
-export default function removeComponents(context = document.documentElement) {
-    queryAll('[g-component]', context).forEach(element => {
-        destroyInstance(element);
+export default function removeComponents(context = document.documentElement, elementID) {
+  ////////////////////////////////////////////////////////////////////////
+  // Check if specific element ID was passed and hence single comp.     //
+  // is intended to be unloaded (i.o. all comp. inside some context)... //
+  ////////////////////////////////////////////////////////////////////////
+
+  if (elementID) {
+    const element = document.getElementById(elementID);
+    destroyInstance(element);
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  // ...otherwise, unload components in specific || global context //
+  ///////////////////////////////////////////////////////////////////
+  else {
+    queryAll("[g-component]", context).forEach((element) => {
+      destroyInstance(element);
     });
+  }
 }
