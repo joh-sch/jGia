db604ce756de4c8ef4366f86328ca50c114ee32c
joh-sch <johannes@schmoll.studio>
2023-10-22 12:23:10 +0200

    impl. supplying of comp. config. received in local comp. config. arg to local comp. instance

--- src/loadComponents.js
+++ src/loadComponents.js
@@ -17,7 +17,13 @@ import logger from "../../../../baseUtilities/logger";
  * @param context   : DOM element
  */
 
-export default function loadComponents(components = {}, context = document.documentElement, elementID, componentConfig) {
+export default function loadComponents(
+  components = {},
+  context = document.documentElement,
+  elementID,
+  componentConfig,
+  localComponentConfigs = {}
+) {
   // Setup...
   const initialisedComponents = [];
   const run_withLogs = config.get("log_custom");
@@ -41,41 +47,44 @@ export default function loadComponents(components = {}, context = document.docum
   if (elementID) {
     logger("info", ["loading single comp. by element ID", `ID: ${elementID}`], "action", { ...logOptions, inline: true });
     //////
-    const element = document.getElementById(elementID);
-    const instance = getComponentFromElement(element);
+    const el = document.getElementById(elementID);
+    const instance = getComponentFromElement(el);
     //////
     if (instance) {
       console.warn("Error: instance exists: \n", instance);
       return true; // continue
     }
     //////
-    const componentName = element.getAttribute("g-component");
+    const componentName = el.getAttribute("g-component");
     //////
     if (typeof components[componentName] === "function") {
-      initialisedComponents.push(createInstance(element, componentName, components[componentName], componentConfig));
-    } else {
-      console.warn('Constructor for component "' + componentName + '" not found.');
-    }
+      initialisedComponents.push(createInstance(el, componentName, components[componentName], componentConfig));
+    } else console.warn('Constructor for component "' + componentName + '" not found.');
   } else {
     ////////////////////////////////////////////////////////////////////
     // ...otherwise, load components in specific || global context... //
     ////////////////////////////////////////////////////////////////////
 
-    queryAll("[g-component]", context).forEach((element) => {
-      const instance = getComponentFromElement(element);
+    queryAll("[g-component]", context).forEach((el) => {
+      const instance = getComponentFromElement(el);
 
       if (instance) {
         console.warn("Error: instance exists: \n", instance);
         return true; // continue
       }
 
-      const componentName = element.getAttribute("g-component");
+      // Get component ID & name...
+      const componentInstanceID = el.id;
+      const componentName = el.getAttribute("g-component");
 
+      // Supply component config. from app config. if available...
+      const componentConfig_fromAppConfig = localComponentConfigs[componentInstanceID];
+      const componentConfig_toUse = componentConfig_fromAppConfig || componentConfig;
+
+      // Instantiate component...
       if (typeof components[componentName] === "function") {
-        initialisedComponents.push(createInstance(element, componentName, components[componentName], componentConfig));
-      } else {
-        console.warn(`Constructor for component "${componentName}" not found.`);
-      }
+        initialisedComponents.push(createInstance(el, componentName, components[componentName], componentConfig_toUse));
+      } else console.warn(`Constructor for component "${componentName}" not found.`);
     });
   }
 
@@ -83,7 +92,5 @@ export default function loadComponents(components = {}, context = document.docum
   // ...call _load/require/mount //
   /////////////////////////////////
 
-  initialisedComponents.forEach((component) => {
-    component._load();
-  });
+  initialisedComponents.forEach((component) => component._load());
 }
